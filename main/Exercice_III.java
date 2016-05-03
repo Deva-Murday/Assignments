5ba91f97bc1ec17c6cb52787612babbf5ca2717b
Deva Murday <deva.murday@yahoo.com>
2016-05-03 11:29:01 +0200

    This class contains the howmany method & average method

--- main/Exercice_III.java
+++ main/Exercice_III.java
@@ -11,15 +11,11 @@ public class Exercice_III {
 		System.out.println("The average is : " + average(number));
 	}
 	public static String howmany(int[] number) {
-		int[] testing = (number);
-		String values = Arrays.toString(testing);
+		String values = Arrays.toString(number);
 		int i = 0;
-		int one = 0;
-		int two = 0;
-		int three = 0;
-		int four = 0;
-		int five = 0;
-		int six = 0;
+		int one = 0;		int two = 0;
+		int three = 0;		int four = 0;
+		int five = 0;		int six = 0;
 		while ( i < values.length() -1 ) {	i++;
 		if (Character.toString(values.charAt(i)).equals("1")) 	{	one++;}
 		if (Character.toString(values.charAt(i)).equals("2")) 	{	two++;}
@@ -33,8 +29,7 @@ public class Exercice_III {
 		return output;
 	}
 public static float average(int[] number) {
-	int[] testing = (number);
-	String values = Arrays.toString(testing);
+	String values = Arrays.toString(number);
 	float result = 0;
 	int i = 0;
 	int howmany = 0;
