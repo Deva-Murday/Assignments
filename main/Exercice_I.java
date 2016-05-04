0ada2bca4d84f9c5a361d1c835e511bbebb07047
Deva Murday <deva.murday@yahoo.com>
2016-05-04 15:50:30 +0200

    Better version

--- main/Exercice_I.java
+++ main/Exercice_I.java
@@ -4,33 +4,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercice_I {
-	static String text, text1, text2, text3;
+	static String text, text2, text3;
+	static String[] text1;
 	static int[] numbers;
 	
 public static void main(String[] args) {	
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Hi, please enter your text, text1,text2 and then your text3 ");
 	text = 		sc.nextLine();
-	text1 = 	sc.nextLine();
-	text2 = 	sc.nextLine();
-	text3 = 	sc.nextLine();
+	
 	
 	reverse(text);
-	System.out.println("The best word in an alphabetical order is : " + alphabetical(text1, text2, text3));
+	// System.out.println("The best word in an alphabetical order is : " + alphabetical(text1, text2, text3));
 	System.out.println("The sorted test is : " + sorted(numbers));}
 
-public static void reverse(String text) {
+public static String reverse(String text) {
 	int i = text.length() - 1;
-	while (i >= 0) {
-		System.out.println(text.charAt(i));
-		i--;
-	}
+	
+	return text;
 }
 	
 public static boolean sorted(int[] numbers) {
 	int[] testing = (numbers);
 	String values = Arrays.toString(testing);
-	
 	Arrays.sort(numbers);
 	String test = Arrays.toString(numbers);
 	
@@ -38,8 +34,10 @@ public static boolean sorted(int[] numbers) {
 	else 						{return false;}
 }
 
-public static String alphabetical(String text1, String text2, String text3) {	
-	char[] array = {text1.charAt(0), text2.charAt(0), text3.charAt(0)};
+
+public static String alphabetical(String[] text1, String text2, String text3) {	
+	char[] array = {text1 , text2.charAt(0), text3.charAt(0)};
+	text2.c(text3);
 	Arrays.sort(array);
 	String[] test = {text1, text2, text3};
 	Arrays.sort(test);
@@ -58,5 +56,5 @@ public static String alphabetical(String text1, String text2, String text3) {
 	{	return text3;	}	
 }
 
-
+*/
 }
