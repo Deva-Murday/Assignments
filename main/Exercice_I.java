9b666b400d15c99a491bbcafc2c11aa8b88378ea
Deva Murday <deva.murday@yahoo.com>
2016-05-12 14:28:13 +0200

    Improved version of the Exercices but there is an error in the best method

--- main/Exercice_I.java
+++ main/Exercice_I.java
@@ -1,27 +1,20 @@
 package main;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Exercice_I {
 	static String text, text2, text3;
 	static String[] text1;
 	static int[] numbers;
 	
-public static void main(String[] args) {	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Hi, please enter your text, text1,text2 and then your text3 ");
-	text = 		sc.nextLine();
-	
-	
-	reverse(text);
-	// System.out.println("The best word in an alphabetical order is : " + alphabetical(text1, text2, text3));
-	System.out.println("The sorted test is : " + sorted(numbers));}
-
 public static String reverse(String text) {
-	int i = text.length() - 1;
-	
-	return text;
+	int i =  text.length() - 1;
+	String reverse = "";
+	while (i >= 0) {
+		String current = Character.toString(text.charAt(i));
+		reverse = reverse + current;	
+		i--;}
+	return reverse;
 }
 	
 public static boolean sorted(int[] numbers) {
@@ -29,32 +22,26 @@ public static boolean sorted(int[] numbers) {
 	String values = Arrays.toString(testing);
 	Arrays.sort(numbers);
 	String test = Arrays.toString(numbers);
-	
-	if (values.equals(test)) 	{return true ;}
-	else 						{return false;}
+	int nb = values.compareTo(test);
+	if (nb == 0) 	{return true ;}
+	else 			{return false;}
 }
 
 
-public static String alphabetical(String[] text1, String text2, String text3) {	
-	char[] array = {text1 , text2.charAt(0), text3.charAt(0)};
-	text2.c(text3);
-	Arrays.sort(array);
+public static String alphabetical(String text1, String text2, String text3) {	
+	String[] array = {text1 , text2, text3};
 	String[] test = {text1, text2, text3};
 	Arrays.sort(test);
-	String sort = Arrays.toString(array);
-	String sorted = Character.toString(sort.charAt(1));
+	String initial = Arrays.toString(array);
+	String sorted = Arrays.toString(test);
 	
-	String val1 = Character.toString(text1.charAt(0));
-	String val2 = Character.toString(text2.charAt(0));
-	if (sorted.equals(val1)) 
+	int nb = initial.compareTo(sorted);
+	if (nb == 0) 
 	{ 	return text1;	}
-	
-	else if (sorted.equals(val2)) 
+	else if (nb > 0)
 	{ 	return text2;	}
 	
-	else 
-	{	return text3;	}	
+	else 	{	return text3;	}
 }
 
-*/
 }
--- main/Exercice_III.java
+++ main/Exercice_III.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 
 public class Exercice_III {
-	static int [] number = {1,2,3,4,5,6,1,3,6,5,4,1,2,3,6};
+	static int [] number = {1,2,3,4,5,6,1,3,6,5,4,1,6,2,3,6};
 	
 	public static void main(String[] args) {
-		System.out.println(howmany(number));
-		System.out.println("The average is : " + average(number));
-	}
+		System.out.println(best(number));
+		System.out.println(findthesix(number));
+			}
 	public static String howmany(int[] number) {
 		String values = Arrays.toString(number);
 		int i = 0;
@@ -33,21 +33,53 @@ public static float average(int[] number) {
 	float result = 0;
 	int i = 0;
 	int howmany = 0;
+	
 	while ( i < values.length() -1 ) 
-	{	if (Character.toString(values.charAt(i)).equals("1") || 
-			Character.toString(values.charAt(i)).equals("2") || 
-			Character.toString(values.charAt(i)).equals("3") ||
-			Character.toString(values.charAt(i)).equals("4") ||
-			Character.toString(values.charAt(i)).equals("5") ||
-			Character.toString(values.charAt(i)).equals("6")) 
-		{	result = result + Integer.parseInt(Character.toString(values.charAt(i))); howmany++;	}
+	{	String cases = Character.toString(values.charAt(i));
+		if (cases.equals("1") || 
+			cases.equals("2") || 
+			cases.equals("3") || 
+			cases.equals("4") || 
+			cases.equals("5") || 
+			cases.equals("6")  )
+		{	result = result + Integer.parseInt(cases); howmany++;	}
 		i++;
 	}
 	result = result / howmany;
 	return result;
 	
-}}
+}
 
+public static int findthesix(int[] number) {
+	String values = Arrays.toString(number);
+	int place = 1;
+	String cases = "";
+	int i = 0;
+	while (i < number.length) {
+	cases = Character.toString(values.charAt(i));
+	if (cases.equals("1") || cases.equals("2") || cases.equals("3") || cases.equals("4") || cases.equals("5")) {
+		place++;i++; 	}
+	else if (!cases.equals("6")) {i++;}}
+	return place;	
+}
+
+
+public static int best(int[] number) {
+	String values = Exercice_III.howmany(number);
+	int i = 0;
+	int current;
+	int best = 0;
+	while (i < values.length()) {
+		String cases = Character.toString(values.charAt(i));
+		if (cases.equals("1") || cases.equals("2") || cases.equals("3") || cases.equals("4") || cases.equals("5") || cases.equals("6")) {
+			current = Integer.parseInt(cases);
+			if (current > best) {	best = current;	}
+			i++;	}
+		else {i++;	}
+	}
+	return best;
+	}
+}
 
 
 
